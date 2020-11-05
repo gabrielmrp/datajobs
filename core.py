2fe8d0f61be3f4a40dfd3c6ddedfca0036fcee08
gabrielmrp <gabrielmrdp@gmail.com>
2020-11-05 16:14:34 -0300

    Filtro Cargo, Categoria

---
+++ core.py
@@ -0,0 +1,110 @@
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 22:31:31 2020
+
+@author: gabri
+"""
+from collections import Counter
+import json
+import re
+import streamlit as st
+
+
+def operation():    
+
+    c={}
+    db = []
+ 
+    #translate terms 
+    def corresp(val,file):
+        
+        with open(file+'.json', encoding='utf-8') as json_file:
+            dict_words = json.load(json_file) 
+         
+        if(val in dict_words):
+            return dict_words[val]
+        else:
+            return val
+    
+    import pandas as pd 
+    #capture used terms in files
+    df_src = pd.DataFrame()
+    
+    for charge in ["BI","CD","ED"]:
+        filename = charge+".json"
+        print(charge)
+        dx = pd.DataFrame()
+        
+        with open(filename, encoding='utf-8') as json_file:
+            data = json.load(json_file)
+        
+        with open("stacklist.json", encoding='utf-8') as stacklist:
+            stack = json.load(stacklist)
+        
+        
+        texts = [re.sub("[/,\():;.]","",d["text"]) for d in data]
+        
+        counts = [] 
+        for t1 in texts:
+            for a in stack:   
+                if( " "+a.lower()+" " in t1.lower()):counts.append(corresp(a,'terms'))
+                 
+        count = []             
+        for i in Counter(counts).most_common():
+            if(i[1]>2):
+                count.append(i)
+                db.append([charge,corresp(i[0],'terms'),corresp(i[0],'categories'),i[1]])
+        
+        
+        c[charge] = count
+        
+        dx = pd.DataFrame(data)
+        
+        dx["charge"]=charge
+        df_src = pd.concat([df_src,dx])
+        
+        
+    with open('categories.json', encoding='utf-8') as json_file:
+            categories = json.load(json_file)    
+        
+    d={};
+    
+    for charge in c.keys():
+        d[charge]={}
+        for item in c[charge]:
+            category = corresp(item[0],'categories') 
+            d[charge][category] = (d[charge][category]+item[1] if category in d[charge] else 0)
+     
+             
+    
+    df = pd.DataFrame()
+    df = df.from_dict(d)
+    
+    df.drop(['Outros'],inplace=True)
+    
+    db_df = pd.DataFrame(db,columns=['charge','item','category','count'])
+    return df,db_df,df_src
+
+#exploratory analisis
+
+
+def watch_not_selected():
+    res=[]    
+    for charge in ["BI","CD","ED"]:
+        filename = charge+".json"    
+        sum([ re.split(" ",t) for t in texts],[])
+        with open(filename, encoding='utf-8') as json_file:
+            data = json.load(json_file)
+        texts = [re.sub("[/,\():;.]","",d["text"]) for d in data]
+        res.append(sum([ re.split(" ",t) for t in texts],[]))
+    
+    all_words = [(x[0].lower(),x[1]) for x in Counter(sum(res,[])).most_common()    ]
+    stack_terms = [x.lower() for x in stack]
+    
+    
+    
+    return [i for i in all_words if i[0] not in stack_terms]
+    
+    
--- interface.py
+++ interface.py
@@ -3,9 +3,60 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import core as core
+import seaborn as sns
+import matplotlib.pyplot as plt
+import base64
+
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}" download="dados.csv">Baixar fonte de dados</a>'
+    return href
+    
 def main():
-    page = st.sidebar.selectbox("Escolha uma opção:", ['Opção 1', 'Opção 2', 'Opção 3'])
-    st.header('Tutorial Streamlit - Heroku')
-    st.write('Esta é uma página de teste.')
+    
+    charge_dict = {'Analista de BI':'BI',
+                   'Cientista de Dados':'CD',
+                   'Eng. de Dados':'BD'
+                    }
+    
+    st.header('Comparação de Cargos na Área de Dados')
+    #st.write('Esta é uma página de teste.')
+    df,db_df,df_src = core.operation()
+    df.columns=['Analista de BI', 'Cientista de Dados', 'Eng. de Dados']
+    df2 = pd.DataFrame(df.unstack()).reset_index()
+    df2.columns=['Cargo','Categoria','Contagem']
+    sb = sns.barplot(x="Categoria", y="Contagem", hue="Cargo", data=df2)
+    sb.set_xticklabels(sb.get_xticklabels(), 
+                          rotation=45, 
+                          horizontalalignment='right')
+    st.pyplot()
+    st.header('Stacks por Cargo e Categoria')
+
+    charge = st.selectbox("Escolha um cargo:", ['Analista de BI', 'Cientista de Dados', 'Eng. de Dados'])
+    
+    category = st.selectbox("Escolha uma categoria:", df.index.tolist())
+    
+    
+    items = db_df[(db_df.charge==charge_dict[charge])&(db_df.category== category )]
+    items = items[['item','count']]
+    items.rename(columns={'item':'Item','count':'Conta'},inplace=True)
+    
+    st.dataframe(items.set_index(['Item']),width=1024, height=768)
+    
+     
+    
+    df_src.rename(columns={'company':'Empresa','text':'Descrição','url':'url','charge':'Cargo'},inplace=True)
+    st.markdown(get_table_download_link(df_src), unsafe_allow_html=True)
+    
 if __name__ == '__main__':
     main()
+    
+    
+    
+     
